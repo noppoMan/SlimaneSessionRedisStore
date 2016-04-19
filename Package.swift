b0620cc5e987303cc10c13e92e9d54213ac0cab8
noppoman <yuki@miketokyo.com>
2016-04-19 18:50:20 +0900

    Rename package name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SlimaneSessionRedisStore",
+    name: "SessionRedisStore",
     dependencies: [
         .Package(url: "https://github.com/noppoMan/swift-redis.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/slimane-swift/SessionMiddleware.git", majorVersion: 0, minor: 1)
