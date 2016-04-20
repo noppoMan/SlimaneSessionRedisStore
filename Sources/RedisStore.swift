f6502b2422ed6c685e2173738a0f3c77f403a5fb
noppoman <yuki@miketokyo.com>
2016-04-21 05:14:12 +0900

    Remove optional

--- Sources/RedisStore.swift
+++ Sources/RedisStore.swift
@@ -12,16 +12,16 @@
 public class RedisStore: SessionStoreType {
 
     private let con: SwiftRedis.Connection
-    
+
     private let serializer: SerializerType
-    
+
     public init(loop: Loop, host: String = "127.0.0.1", port: UInt = 6379, expires: Int = (60 * 30), serializer: SerializerType = JsonSerializer()) throws {
         self.con = try Connection(loop: loop.loopPtr, host: host, port: port)
         self.serializer = serializer
     }
-    
+
     // TODO need to change completion to GenericResult<[String : Any?]> -> ()
-    public func load(sessionId: String, completion: (SessionResult<[String: AnyObject?]>) -> Void) {
+    public func load(sessionId: String, completion: (SessionResult<[String: AnyObject]>) -> Void) {
         Redis.command(self.con, command: .GET(sessionId)) { [unowned self] result in
             if case .Success(let repl) = result {
                 if repl == "" {
@@ -37,25 +37,18 @@ public class RedisStore: SessionStoreType {
             }
         }
     }
-    
-    public func store(sessionId: String, values: [String : AnyObject?], expires: Int?, completion: () -> Void) {
-        var unwrapedValues: [String: AnyObject] = [:]
-        for (key, value) in values {
-            if let v = value {
-                unwrapedValues[key] = v
-            }
-        }
-        
+
+    public func store(sessionId: String, values: [String: AnyObject], expires: Int?, completion: () -> Void) {
         do {
-            let sesValue = try self.serializer.serialize(unwrapedValues)
-            
+            let sesValue = try self.serializer.serialize(values)
+
             let command: Commands
             if let expires = expires {
                 command = .SETEX(sessionId, expires, sesValue)
             } else {
                 command = .SET(sessionId, sesValue)
             }
-            
+
             // Need to handle error
             Redis.command(con, command: command)
             completion()
@@ -63,7 +56,7 @@ public class RedisStore: SessionStoreType {
             return completion()
         }
     }
-    
+
     public func destroy(sessionId: String) {
         Redis.command(con, command: .DEL([sessionId]))
     }
