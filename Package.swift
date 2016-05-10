4cbad76aca75f22d76a897698641c90631398ae5
noppoman <yuki@miketokyo.com>
2016-05-11 02:55:42 +0900

    update dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SessionRedisStore",
     dependencies: [
-        .Package(url: "https://github.com/noppoMan/swift-redis.git", majorVersion: 0),
-        .Package(url: "https://github.com/slimane-swift/SessionMiddleware.git", majorVersion: 0)
+        .Package(url: "https://github.com/noppoMan/swift-redis.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/slimane-swift/SessionMiddleware.git", majorVersion: 0, minor: 3)
     ]
 )
